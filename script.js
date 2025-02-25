6f925742cd37e9eeadecdc213facf03992e4582e
kasrarag25 <kasracalshvin@gmail.com>
2025-02-25 16:06:29 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-window.onload = function() {
-    setTimeout(function() {
-        document.querySelector('.loading-container').style.display = 'none'; // مخفی کردن لودینگ
-        document.getElementById('game-container').style.display = 'block'; // نمایش دکمه بازی
-    }, 5000); // پس از 5 ثانیه
-};
+let coins = 0;
 
-function startGame() {
-    // تابعی که بعد از کلیک روی دکمه "شروع بازی" اجرا می‌شود.
-    window.location.href = "https://yourgameurl.com";  // لینک به صفحه بازی
+function collectCoin() {
+    coins++;
+    document.getElementById('coins-count').textContent = coins;
 }
